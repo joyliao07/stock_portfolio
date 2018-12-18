3778e377fa77b645d29feeb9bda9fb5d1378298a
Joyce Liao <joyliao07@gmail.com>
2018-12-17 23:04:22 -0800

    tests pass 92%

--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -8,6 +8,7 @@ class TestClass:
         pass
 
     def test_home_route_get(self, app):
+        """To test / with 200."""
         # QUESTION - RV GET DROPPED AFTER THE FIRST ASSERT??????
         rv = app.test_client().get('/')
         assert rv.status_code == 200
@@ -15,29 +16,26 @@ class TestClass:
 
 
     def test_home_route_post(self, app):
+        """Test home route."""
         rv = app.test_client().post('/')
         assert rv.status_code == 405
 
 
     def test_search_route_get_without_redirects(self, app):
-            rv = app.test_client().get('/search')
-            assert rv.status_code == 302
-            # rv = app.test_client().get('/search')
+        """To test /search with 302."""
+        rv = app.test_client().get('/search')
+        assert rv.status_code == 302
+        # rv = app.test_client().get('/search')
 
 
     def test_search_route_get_with_redirect(self, app):
-            rv = app.test_client().get('/search', follow_redirects=True)
-            assert rv.status_code == 200
+        """To test /search with 200."""
+        rv = app.test_client().get('/search', follow_redirects=True)
+        assert rv.status_code == 200
 
 
-    def test_protected_route_with_user_fixture(self, app, user):
-        """
-        Using the user fixture's id
-        add it to the current session.
-        This should trigger the auth to load user with the given user_id
-        add store it in 'g'
-        after that protected routes should be reachable
-        """
+    def test_search_get_method(self, app, user):
+        """To test /search with 200."""
         # TO TEST ROUTES.PY LINE 33, 52-54, 69-71
 
         # use 'with' statement to keep context
@@ -60,6 +58,7 @@ class TestClass:
 
 
     def test_search_with_post(self, app, user, portfolio, company):
+        """To test /search with 200."""
         # TO TEST ROUTES.PY LINE 25, 55 - 67
 
         with app.test_client() as client:
@@ -73,6 +72,7 @@ class TestClass:
 
 
     def test_search_with_post_404(self, app, user, portfolio, company):
+        """To test /search with invalid company symbol."""
         # TO TEST ROUTES.PY LINE 67 - 69.. does not hit the lines
 
         with app.test_client() as client:
@@ -85,6 +85,7 @@ class TestClass:
 
 
     def test_preview_get(self, app, user, portfolio, company):
+        """To test /preivew with valid company symbol."""
         # TO TEST ROUTES.PY LINE 80-97, 119-120
 
         with app.test_client() as client:
@@ -98,9 +99,7 @@ class TestClass:
 
 
     def test_add_fresh_company_to_portfolio_redirect(self, authenticated_client, portfolio):
-        """
-        Add a fresh company and follow redirect
-        """
+        """Add a fresh company and follow redirect"""
 
         # need to first post from search page in order to add company stock symbol to session
         authenticated_client.post('/search', data={'symbol': 'goog'}, follow_redirects=True)
@@ -108,75 +107,31 @@ class TestClass:
         # simulate form input
         form_data = {'symbol': 'goog', 'companyName': 'Alphabet Inc.', 'portfolios': portfolio.id}
 
-        # post the form data and follow redirect
         rv = authenticated_client.post('/preview', data=form_data, follow_redirects=True)
-
-        # confirm status
         assert rv.status_code == 200
 
         # confirm proper markup
         assert b'<h2>Search for stocks</h2>' in authenticated_client.post('/preview', data=form_data, follow_redirects=True).data
 
     def test_portfolio_add_new(self, authenticated_client, portfolio):
-        """
-        Add a fresh company and follow redirect
-        """
-
-        # need to first post from search page in order to add company stock symbol to session
-        # authenticated_client.post('/search', data={'symbol': 'goog'}, follow_redirects=True)
-
+        """Add a fresh portfolio and follow redirect"""
         # simulate form input
         form_data = {'name': 'new investment'}
 
-        # post the form data and follow redirect
         rv = authenticated_client.post('/portfolio', data=form_data, follow_redirects=True)
 
-        # confirm status
         assert rv.status_code == 200
 
-        # confirm proper markup
-        # assert b'<h2>Search for stocks</h2>' in authenticated_client.post('/preview', data=form_data, follow_redirects=True).data
+    def test_registration_200(self, client):
+        """To test /register with 200."""
+        res = client.get('/register')
+        assert res.status_code == 200
 
-
-
-
-
-
-
-
-
-    # NOT SURE WHAT IT IS................................................
-    # def test_search_with_incorrect_company_symbol(self, app, user):
-    #     """
-    #     Using the user fixture's id
-    #     add it to the current session.
-    #     This should trigger the auth to load user with the given user_id
-    #     add store it in 'g'
-    #     after that protected routes should be reachable
-    #     """
-
-    #     # use 'with' statement to keep context
-    #     with app.test_client() as client:
-
-    #         # set session's user_id
-    #         with client.session_transaction() as flask_session:
-    #             flask_session['user_id'] = user.id
-
-    #         # navigate to protected route
-    #         rv = client.get('/search')
-
-    #         # auth module's @app.before_request should result in
-    #         # g.user being set
-    #         assert g.user == user
-
-    #         # good login gives 200
-    #         assert rv.status_code == 200
-
-
-
-
-
-
-
-
-
+    def test_registration_with_actual_name_and_password(self, client):
+        """To test /register with POST."""
+        res = client.post(
+            '/register',
+            data={'email': 'test@example.com', 'password': 'seekret'},
+            follow_redirects=True,
+        )
+        assert res.status_code == 200
